bd2dedae78b43134217aada30d48a13ea726011c
vince <vincedinardo21@gmail.com>
2022-05-27 15:19:11 +0200

    Stampo nomi users

--- js/myscript.js
+++ js/myscript.js
@@ -45,7 +45,139 @@ const myApp = new Vue({
                         stato : "received"
                     },
                 ]
-            }
+            },
+            {
+                name : "Orazio",
+                avatar : 2,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Ciao Domiziana",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Oggi a che ora ci vediamo?",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Mi dispiace, oggi non posso",
+                        stato : "received"
+                    },
+                ]
+            },
+            {
+                name : "Alice",
+                avatar : 2,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Ciao Domiziana",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Oggi a che ora ci vediamo?",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Mi dispiace, oggi non posso",
+                        stato : "received"
+                    },
+                ]
+            },
+            {
+                name : "Guglielmo",
+                avatar : 2,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Ciao Domiziana",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Oggi a che ora ci vediamo?",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Mi dispiace, oggi non posso",
+                        stato : "received"
+                    },
+                ]
+            },
+            {
+                name : "Jessico Calcetto",
+                avatar : 2,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Ciao Domiziana",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Oggi a che ora ci vediamo?",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Mi dispiace, oggi non posso",
+                        stato : "received"
+                    },
+                ]
+            },
+            {
+                name : "Gennaro",
+                avatar : 2,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Ciao Domiziana",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Oggi a che ora ci vediamo?",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Mi dispiace, oggi non posso",
+                        stato : "received"
+                    },
+                ]
+            },
+            {
+                name : "Tizio di Fortnite",
+                avatar : 2,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Ciao Domiziana",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Oggi a che ora ci vediamo?",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Mi dispiace, oggi non posso",
+                        stato : "received"
+                    },
+                ]
+            },
         ]
     }
 });
