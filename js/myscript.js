f3072f8892aa07cfa72df9ac7785fc68d60eca42
vince <vincedinardo21@gmail.com>
2022-05-27 12:33:28 +0200

    Struttura dati

---
+++ js/myscript.js
@@ -0,0 +1,51 @@
+const myApp = new Vue({
+    el : "#app",
+    data : {
+        contacts : [
+            {
+                name : "Teodorico",
+                avatar : 1,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Hey, Teo",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Ho saputo che hai trovato un lavoro!",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Si, ma mi hanno licenziato subito",
+                        stato : "received"
+                    },
+                ]
+            },
+            {
+                name : "Domiziana",
+                avatar : 2,
+                visible : true,
+                messages : [
+                    {
+                        date : "10/12/2022 15:30:55",
+                        message : "Ciao Domiziana",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 20:30:58",
+                        message : "Oggi a che ora ci vediamo?",
+                        stato : "sent"
+                    },
+                    {
+                        date : "10/12/2022 22:35:58",
+                        message : "Mi dispiace, oggi non posso",
+                        stato : "received"
+                    },
+                ]
+            }
+        ]
+    }
+});
